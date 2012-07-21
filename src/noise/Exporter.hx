2bba85b60901b6d20ac29b8a831f84ca6cc2883e
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-07-21 19:55:48 +0100

    Improve noise exporter. Add a C program to do a brute-force comparison of exported noise against various PRNGs.

--- src/noise/Exporter.hx
+++ src/noise/Exporter.hx
@@ -1,25 +1,65 @@
 package noise;
 
 import flash.geom.Rectangle;
-import flash.events.Event;
 import flash.display.BitmapData;
+
 class Exporter {
+    static inline var WIDTH = 16384;
+    static inline var HEIGHT = 16384;
+
+    static inline var PIXELS_AT_ONCE = 1024;
+
     static var noise:BitmapData;
-    static var noiseY:Int;
+    static var x = 0;
+    static var y = 0;
+    static var rect = new Rectangle();
 
     static function main() {
-        noise = new BitmapData(16384, 16384);
-        noise.noise(3, 0, 255, 15);
+        noise = new flash.display.BitmapData(WIDTH, HEIGHT);
+        noise.noise(3, 0, 255, 7);
 
-        flash.Lib.current.stage.addEventListener(Event.ENTER_FRAME, exportNoise);
+        flash.Lib.current.stage.addEventListener(flash.events.Event.ENTER_FRAME, exportNoise);
     }
 
-    static function exportNoise(event:Event) {
-        var rect = new Rectangle(0, noiseY++, noise.width, 1);
-        trace(noise.getVector(rect).join(","));
+    static function exportNoise(event:flash.events.Event) {
+        var startTime = flash.Lib.getTimer();
+        var endTime = startTime + 950/flash.Lib.current.stage.frameRate;
 
-        if (noiseY >= noise.height) {
-            flash.Lib.current.stage.removeEventListener(Event.ENTER_FRAME, exportNoise);
+        var timeOut = flash.Lib.getTimer() >= endTime;
+        while (!timeOut && y < noise.height) {
+            while (!timeOut && x < noise.width) {
+                var targetX = x + PIXELS_AT_ONCE;
+
+                if (targetX > noise.width) {
+                    targetX = noise.width;
+                }
+
+                rect.x = x;
+                rect.y = y;
+                rect.width = targetX - x;
+                rect.height = 1;
+
+                var pixels = noise.getVector(rect);
+
+                flash.Lib.trace(pixels.join("\n"));
+
+                x = targetX;
+
+                timeOut = flash.Lib.getTimer() >= endTime;
+            }
+
+            if (!timeOut) {
+                x = 0;
+                ++y;
+            }
         }
+
+        var progress = (y * noise.width + x) / (noise.width * noise.height);
+
+        var g = flash.Lib.current.graphics;
+        g.clear();
+        g.beginFill(if (progress >= 1) 0x00ff00 else 0x000000);
+        g.drawRect(0, 0, flash.Lib.current.stage.stageWidth * progress, flash.Lib.current.stage.stageHeight);
+        g.endFill();
     }
 }
---
+++ src/noise/compare-dump.c
@@ -0,0 +1,200 @@
+#include <stdio.h>
+
+#define SEED 3
+
+typedef int (* generator_t)();
+
+/* Linear congruential generators */
+
+/* Numerical Recipes */
+int lcg1() {
+    static unsigned int a = 1664525;
+    static unsigned int c = 1013904223;
+    static unsigned int x = SEED;
+
+    x = (a * x + c) & 0xffffffffu;
+    return x & 0xff;
+}
+
+/* Borland C lrand() */
+int lcg2() {
+    static unsigned int a = 22695477;
+    static unsigned int c = 1;
+    static unsigned int x = SEED;
+
+    x = (a * x + c) & 0xffffffffu;
+    return x & 0xff;
+}
+
+/* Borland C rand() */
+int lcg3() {
+    static unsigned int a = 22695477;
+    static unsigned int c = 1;
+    static unsigned int x = SEED;
+
+    x = (a * x + c) & 0xffffffffu;
+    return x >> 16 & 0xff;
+}
+
+/* glibc */
+int lcg4() {
+    static unsigned int a = 1103515245;
+    static unsigned int c = 12345;
+    static unsigned int x = SEED;
+
+    x = (a * x + c) & 0x8fffffffu;
+    return x & 0xff;
+}
+
+/* ANSI C: Watcom, Digital Mars, CodeWarrior, IBM VisualAge C */
+int lcg5() {
+    static unsigned int a = 1103515245;
+    static unsigned int c = 12345;
+    static unsigned int x = SEED;
+
+    x = (a * x + c) & 0x8fffffffu;
+    return x >> 16 & 0xff;
+}
+
+/* Delphi/Borland Pascal */
+int lcg6() {
+    static unsigned int a = 134775813;
+    static unsigned int c = 1;
+    static unsigned int x = SEED;
+
+    x = (a * x + c) & 0xffffffffu;
+    return x >> 16 & 0xff;
+}
+
+/* Visual C */
+int lcg7() {
+    static unsigned int a = 214013;
+    static unsigned int c = 2531011;
+    static unsigned int x = SEED;
+
+    x = (a * x + c) & 0xffffffffu;
+    return x >> 16 & 0xff;
+}
+
+/* Visual Basic <= 6 */
+int lcg8() {
+    static unsigned int a = 1140671485;
+    static unsigned int c = 12820163;
+    static unsigned int x = SEED;
+
+    x = (a * x + c) & 0xffffffu;
+    return x & 0xff;
+}
+
+/* RtlUniform from Native API */
+int lcg9() {
+    static unsigned int m = (1 << 31) - 1;
+    static unsigned int a = 2147483629;
+    static unsigned int c = 2147483587;
+    static unsigned int x = SEED;
+
+    x = (a * x + c) % m;
+    return x & 0xff;
+}
+
+/* CarbonLib */
+int lcg10() {
+    static unsigned int m = (1 << 31) - 1;
+    static unsigned int a = 16807;
+    static unsigned int c = 0;
+    static unsigned int x = SEED;
+
+    x = (a * x + c) % m;
+    return x & 0xff;
+}
+
+/* MMIX */
+int lcg11() {
+    static unsigned long a = 6364136223846793005ul;
+    static unsigned long c = 1442695040888963407ul;
+    static unsigned long x = SEED;
+
+    x = (a * x + c) & 0xfffffffffffffffful;
+    return x & 0xff;
+}
+
+/* VAX MTH$RANDOM, old glibc */
+int lcg12() {
+    static unsigned int a = 69069;
+    static unsigned int c = 1;
+    static unsigned int x = SEED;
+
+    x = (a * x + c) & 0xffffffffu;
+    return x & 0xff;
+}
+
+/* java.util.Random */
+int lcg13() {
+    static unsigned long a = 25214903917ul;
+    static unsigned long c = 11ul;
+    static unsigned long x = SEED;
+
+    x = (a * x + c) & 0xfffffffffffful;
+    return x >> 16 & 0xff;
+}
+
+/* LC53 in Forth */
+int lcg14() {
+    static unsigned int m = -5;
+    static unsigned int a = -333333333;
+    static unsigned int c = 0;
+    static unsigned int x = SEED;
+
+    x = (a * x + c) % m;
+    return x & 0xff;
+}
+
+
+/* Main */
+
+void main() {
+    generator_t generators[] = {
+        &lcg1, &lcg2, &lcg3, &lcg4, &lcg5, &lcg6, &lcg7, &lcg8, &lcg9, &lcg10,
+        &lcg11, &lcg12, &lcg13, &lcg14
+    };
+
+    int num_generators = sizeof(generators) / sizeof(generator_t);
+
+    int generator_active[sizeof(generators) / sizeof(generator_t)];
+
+    int generator_index, position, matches, value, byte, generated_byte;
+
+    for (generator_index=0; generator_index<num_generators; ++generator_index) {
+        generator_active[generator_index] = 1;
+    }
+
+    position = 0;
+    while ((matches = scanf("%d\n", &value)) >= 0) {
+        if (matches == 0) {
+            getchar();
+        } else {
+            do {
+                byte = (value >> (16 - (position % 3) * 8)) & 0xff;
+
+                for (generator_index=0; generator_index<num_generators; ++generator_index) {
+                    if (generator_active[generator_index]) {
+                        generated_byte = generators[generator_index]();
+                        if (generated_byte != byte) {
+                            generator_active[generator_index] = 0;
+                            printf("Generator %d didn't match at position %d: source: %d, generated: %d\n",
+                                    generator_index, position, byte, generated_byte);
+                        }
+                    }
+                }
+
+                ++position;
+            } while (position % 3 != 0);
+        }
+    }
+
+    for (generator_index=0; generator_index<num_generators; ++generator_index) {
+        if (generator_active[generator_index]) {
+            printf("Generator %d matched the sequence of %d bytes!\n", generator_index, position);
+        }
+    }
+}
